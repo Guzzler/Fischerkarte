05f79e4aebb2c2ae2370266ad876aa08134bb95a
Guzzler <sharangpai123@gmail.com>
2020-07-30 23:18:20 +0530

    commit build folder

---
+++ build/precache-manifest.7604a28527b9dbe631672ecf1ac78673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0561fe47d50d63af685702f0b71739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c67c4070b10e74fcd6bf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c82d4c018debb2ca1331",
+    "url": "/static/js/2.97e70642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.97e70642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67c4070b10e74fcd6bf",
+    "url": "/static/js/main.70296e8c.chunk.js"
+  },
+  {
+    "revision": "dd8d78bbe104135832ec",
+    "url": "/static/js/runtime-main.25ac6373.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
